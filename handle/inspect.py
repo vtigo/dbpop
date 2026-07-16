b653ee585934b7990fd4f5fab7e80ef750624f72
vtigo <rf.tigas@gmail.com>
2026-07-15 22:39:55 -0300

    introspection output

--- handle/inspect.py
+++ handle/inspect.py
@@ -1,14 +1,16 @@
 from database.api import Database
 
-# TODO: extract configuration
-DATABASE_URL = "postgresql+psycopg2://appuser:changeme@localhost:5432/dbpop-probe"
+def print_introspection(table_names: list[str]):
+    for table_name in inspector.get_table_names(schema="public"):
+        print(table_name, "-" * len(table_name), sep="\n")
+        columns = inspector.get_columns(table_name, schema="public")
+        print(*(f"{c['name']} {c['type']}" for c in columns), sep="\n", end="\n\n")
+        print("PKs:", inspector.get_pk_constraint(table_name), end="\n\n")
+        print("FKs:", inspector.get_foreign_keys(table_name), end="\n\n")
+        print("Indexes:", inspector.get_indexes(table_name), end="\n\n\n")
 
-inspector = Database(DATABASE_URL).inspector()
-
-for table_name in inspector.get_table_names(schema="public"):
-    print(table_name)
-    for column in inspector.get_columns(table_name, schema="public"):
-        print(" ", column["name"], column["type"])
-    print(" PKs:", inspector.get_pk_constraint(table_name))
-    print(" FKs:", inspector.get_foreign_keys(table_name))
-    print(" Indexes:", inspector.get_indexes(table_name))
+if __name__ == "__main__":
+    db_url = "postgresql+psycopg2://appuser:changeme@localhost:5432/dbpop-probe"
+    inspector = Database(db_url).inspector()
+    table_names = inspector.get_table_names(schema="public")
+    print_introspection(table_names)
